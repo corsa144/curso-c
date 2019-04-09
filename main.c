ca264b53567b713bb3469388b2c5e2827daa3513
corsa144 <40776473+corsa144@users.noreply.github.com>
2019-04-08 21:23:53 -0300

    clase6_nombreyapellido
    strcat

--- main.c
+++ main.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
     char nombre[31];
+    char apellido[31];
+    char nombreYapellido[62];
 
-    printf("Ingrese un nombre: \n");
-    fflush(stdin);
-    //scanf("%s",nombre);
-    gets(nombre);
-    printf("El nombre es: %s\n",nombre);
+
+    printf("Ingrese nombre:\n");
+    fgets(nombre,sizeof(nombre)-2,stdin);
+    printf("Ingrese apellido:\n");
+    fgets(apellido,sizeof(apellido)-2,stdin);
+    strlwr(nombre);
+    strlwr(apellido);
+    //strupr(nombre[0]);
+   // strupr(apellido[0]);
+    strcat(nombreYapellido,nombre);
+    strcat(nombreYapellido,apellido);
+    printf("el nombre y apellido es: %s",nombreYapellido);
     return 0;
 }
